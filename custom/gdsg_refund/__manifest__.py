f5b3fa3631a2114e6f30ec52a69ee871fdec463b
SONTQ\tqson <goddreject198>
2024-07-02 22:00:38 +0700

    no message

--- custom/gdsg_refund/__manifest__.py
+++ custom/gdsg_refund/__manifest__.py
@@ -8,6 +8,7 @@
         'security/ir.model.access.csv',
         'views/gdsg_refund_rule_category_view.xml',
         'views/gdsg_refund_rule_view.xml',
+        'views/gdsg_refund_rate_view.xml',
         'views/gdsg_refund_structure_view.xml',
         'menus/gdsg_refund_menu.xml',
     ],
---
+++ custom/gdsg_refund/models/gdsg_refund_rate.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api, tools, _
+
+
+class Refund_Rate(models.Model):
+    _name = 'gdsg_refund.rate'
+    _description = 'Refund Rate'
+
+    name = fields.Char('Customer', required=True)
+    contract_tuition = fields.Char('Contract Tuition')
+    tuition_fee = fields.Integer('Tuition')
+    material = fields.Integer('Material')
+    total_amount = fields.Integer('Total')
+    description = fields.Char('Description')
+    retains = fields.Integer('Retains')
+    refund_rate = fields.Integer('Refund rate (%)')
+    assistant_support = fields.Integer('Assistant Support')
+    teacher_remuneration = fields.Integer('Teacher remuneration')
+    outside = fields.Integer('Outside')
+    tax = fields.Integer('Tax (%)')
+
+
+
